6dd7e093236212b026e3e001f7319c7b9ecec66b
Faize Erturk <86715389+faizerturk@users.noreply.github.com>
2023-11-30 16:51:15 +0300

    initial setup

---
+++ ssr-example/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = React.useState(0);
+
+  return (
+    <div>
+      <p></p>
+    </div>
+  );
+}
+
+export default App;
